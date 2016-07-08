4e35b91179d03b65621e601fd92ea0cdaec5babb
Alejandro Pugliesi Soliz <apugliesi@tecorp.com.bo>
2016-07-08 17:58:12 -0400

    Se adicionaron codigo para verificar el fallo o acierto de la adivinanza de la palabra secreta. el codigo actualmente controla el numero de intentos que le queda al jugador hasta terminar el juego. 	modified:   lib/ahorcado.rb 	modified:   spec/ahorcado_spec.rb

--- spec/ahorcado_spec.rb
+++ spec/ahorcado_spec.rb
@@ -4,4 +4,21 @@ describe Ahorcado do
 		ahorcado = Ahorcado.new
 		ahorcado.palabra_secreta.should == "elefante"
 	end 
+	it "deberia tener 6 intentos cuando inicia el juego " do 
+		ahorcado = Ahorcado.new
+		ahorcado.intentos.should == 6
+	end
+	it "deberia retornar true cuando acierta una letra de la palabra secreta " do 
+		ahorcado = Ahorcado.new
+		ahorcado.adivinar("a").should == true
+	end
+	it "deberia retornar false cuando acierta una letra de la palabra secreta " do 
+		ahorcado = Ahorcado.new
+		ahorcado.adivinar("b").should == false
+	end
+	it "deberia descontar un intento cuando no acierta una letra de la secreta " do 
+		ahorcado = Ahorcado.new
+		ahorcado.adivinar("b")
+		ahorcado.intentos.should == 5
+	end
 end
