bd2c5c0feef2e02d9c14c08ddde3727b509684a9
Alejandro Pugliesi Soliz <apugliesi@tecorp.com.bo>
2016-07-09 09:52:26 -0400

    se realizo el algoritmo de control de vector llenado del vector resultante vs vector de palabra secreta. el metodo adivinar ya no devuelve true o false cuando adivina, sino que devuelve el vector resultante segun se haya adivinado y descuenta o no vidas. 	modified:   lib/ahorcado.rb 	modified:   spec/ahorcado_spec.rb

--- lib/ahorcado.rb
+++ lib/ahorcado.rb
@@ -1,11 +1,20 @@
 class Ahorcado
 	def initialize 
 		@secret = "elefante"
+		@vector_secreto = @secret.split("")
+		@vector_resolucion= []
+		@vector_secreto.length.times do
+			@vector_resolucion.push("")
+		end
 		@vidas = 6
 	end
 
 	def palabra_secreta
 		@secret
+	end
+	
+	def vector_secreto
+		@vector_secreto
 	end
 
 	def vidas
@@ -14,11 +23,17 @@ class Ahorcado
 
 	def adivinar(letra)
 		if (@secret.include? letra)
-			return true
+		    posicion = 0
+			@vector_secreto.each do |letra_secreta|
+				if(letra == letra_secreta)
+					@vector_resolucion[posicion] = letra
+				end 
+				posicion += 1
+			end
 		else
 			@vidas -= 1
-			return false
 		end
+		@vector_resolucion
 	end  
 
 end
--- spec/ahorcado_spec.rb
+++ spec/ahorcado_spec.rb
@@ -8,17 +8,30 @@ describe Ahorcado do
 		ahorcado = Ahorcado.new
 		ahorcado.vidas.should == 6
 	end
-	it "deberia retornar true cuando acierta una letra de la palabra secreta " do 
-		ahorcado = Ahorcado.new
-		ahorcado.adivinar("a").should == true
-	end
-	it "deberia retornar false cuando acierta una letra de la palabra secreta " do 
-		ahorcado = Ahorcado.new
-		ahorcado.adivinar("b").should == false
-	end
+
+	#it "deberia retornar true cuando acierta una letra de la palabra secreta " do 
+	#	ahorcado = Ahorcado.new
+	#	ahorcado.adivinar("a").should == true
+	#end
+	#it "deberia retornar false cuando acierta una letra de la palabra secreta " do 
+	#	ahorcado = Ahorcado.new
+	#	ahorcado.adivinar("b").should == false
+	#end
 	it "deberia descontar un intento cuando no acierta una letra de la secreta " do 
 		ahorcado = Ahorcado.new
 		ahorcado.adivinar("b")
 		ahorcado.vidas.should == 5
+	end
+	it "deberia retornar la palabra secreta como vector " do 
+		ahorcado = Ahorcado.new
+		ahorcado.vector_secreto.should == ["e","l","e","f","a","n","t","e"]
+	end
+	it "deberia retornar vector vacio cuando no adivine" do 
+		ahorcado = Ahorcado.new
+		ahorcado.adivinar("b").should == ["","","","","","","",""]
+	end
+	it "deberia retornar vector con letras  cuando  adivine" do 
+		ahorcado = Ahorcado.new
+		ahorcado.adivinar("e").should == ["e","","e","","","","","e"]
 	end
 end
